4b17d2ace09f7f8a6d2ac7fabdc3defb1d43e52e
hiratara <hiratara@eef97cb5-7a25-4bbc-a59f-8993798d106f>
2008-03-15 15:31:46 +0000

    英語表記に修正。Criticがないと怒られてるみたいなので修正。
    git-svn-id: http://mini/svn/public/perl/lib/WWW-YahooJapan-KanaAddress@24 eef97cb5-7a25-4bbc-a59f-8993798d106f

--- lib/WWW/YahooJapan/KanaAddress.pm
+++ lib/WWW/YahooJapan/KanaAddress.pm
@@ -6,7 +6,7 @@ use warnings;
 use strict;
 use Carp;
 
-use version; our $VERSION = qv('0.1.0');
+use version; our $VERSION = qv('0.1.1');
 
 use LWP::UserAgent;
 use URI::Escape;
@@ -65,157 +65,155 @@ my $search_url_tpl = 'http://search.map.yahoo.co.jp/search?p=%s&ei=euc-jp';
 my $kana_url_tpl   = 'http://map.yahoo.co.jp/address?ac=%s';
 
 
-# コンストラクタ
+# a constructor
 sub new{ 
-	my $class = shift;
+    my $class = shift;
 
-	my %opt = ref($_[0]) ? %{$_[0]} : @_;
+    my %opt = ref($_[0]) ? %{$_[0]} : @_;
 
-	my $ua = $opt{ua} || LWP::UserAgent->new();
+    my $ua = $opt{ua} || LWP::UserAgent->new();
 
-	bless {ua => $ua}, $class;
+    bless {ua => $ua}, $class;
 };
 
 
-# 概要: タグを取り除く
-# 引数: HTML
-# 戻値: タグを除いた文字列
+# strip tags from HTML
 sub _strip_tag{
-	my $self = shift;
-	my @strs = @_;
-	return map { my $s = $_; $s =~ s/<[^>]+>//g; $s; } @strs;
+    my $self = shift;
+    my @strs = @_;
+    return map { my $s = $_; $s =~ s/<[^>]+>//g; $s; } @strs;
 }
 
 
-# 概要: Yahoo Mapsにて、自由文検索を行う
-# 引数: 検索語
-# 戻値: 結果ページのHTML(無加工)
+# search from Yahoo Maps by free word.
+# the return value is raw html.
 sub _do_freeword_search{
-	my $self = shift;
-	my $word = shift;
+    my $self = shift;
+    my $word = shift;
 
-	my $url = sprintf($search_url_tpl, uri_escape($word));
+    my $url = sprintf($search_url_tpl, uri_escape($word));
 
-	my $ua  = $self->{ua};
-	my $res = $ua->get($url);
-	return $res->content();
+    my $ua  = $self->{ua};
+    my $res = $ua->get($url);
+    return $res->content();
 }
 
 
-# 概要: 町字文字列を、Yahooで扱う町字文字列に変換
-# 引数: 都道府県, (Yahoo書式の)市区文字列, 町字文字列
-# 戻値: (Yahoo書式の)町字文字列
+# change CHO-AZA into CHO-AZA of Yahoo expression.
+# params: TO-DO-FU-KEN, SHI-KU(Yahoo exp.), CHO-AZA
+# return: CHO-AZA(Yahoo exp.)
 sub _correct_choaza{
-	my $self = shift;
-	my ($tdfk, $corrected_shiku, $choaza) = @_;
+    my $self = shift;
+    my ($tdfk, $corrected_shiku, $choaza) = @_;
 
-	my $html = $self->_do_freeword_search($tdfk . $corrected_shiku . $choaza);
+    my $html = $self->_do_freeword_search($tdfk . $corrected_shiku . $choaza);
 
-	# 検索結果の一番最初を取る
-	my $corrected_address = undef;
-	if ($html =~ 
-	       m{<a [^>]+http://map\.yahoo\.co\.jp/pl\?p=[^>]+>(.+?)</a>}) {
-		$corrected_address = $1;
-	}else{
-		die "can't find $corrected_shiku, $choaza .";
-	}
+    # use first element of the search result.
+    my $corrected_address = undef;
+    if ($html =~ 
+        m{<a [^>]+http://map\.yahoo\.co\.jp/pl\?p=[^>]+>(.+?)</a>}) {
+        $corrected_address = $1;
+    }else{
+        die "can't find $corrected_shiku, $choaza .";
+    }
 
-	# 市区部分を除く
-	my $regex = quotemeta($tdfk. $corrected_shiku);
-	$corrected_address =~ s/^$regex//;
+    # chop SHI-KU
+    my $regex = quotemeta($tdfk. $corrected_shiku);
+    $corrected_address =~ s/^$regex//;
 
-	# 番地を省く
-	$corrected_address =~ s/[0-9]+$//;
+    # chop address-number
+    $corrected_address =~ s/[0-9]+$//;
 
-	# 丁目以降を除く
-	$corrected_address =~ s/[0-9]+丁目$//;
+    # chop chome
+    $corrected_address =~ s/[0-9]+丁目$//;
 
-	return $corrected_address;
+    return $corrected_address;
 }
 
 
-# 概要: 市区文字列を、Yahooで扱う市区文字列に変換
-# 引数: 都道府県文字列, 市区文字列
-# 戻値: 市区ID, (Yahoo書式の)市区文字列
+# change SHI-KU into SHI-KU of Yahoo expression.
+# params: TO-DO-FU-KEN, SHI-KU
+# return: SHI-KU ID, SHI-KU(Yahoo exp.)
 sub _correct_shiku{
-	my $self = shift;
-	my ($tdfk, $shiku) = @_;
+    my $self = shift;
+    my ($tdfk, $shiku) = @_;
 
-	my $html = $self->_do_freeword_search($tdfk . $shiku);
+    my $html = $self->_do_freeword_search($tdfk . $shiku);
 
-	my @codes = ();
-	while ($html =~ 
-	       m{<a [^>]+map\.yahoo\.co\.jp/address\?ac=(\d+)[^>]+>(.+?)</a>}g) {
-		push(@codes, [$1, $2]);
-	}
-	die "can't determine codes: " . join(',', @codes) if(@codes != 1);
+    my @codes = ();
+    while ($html =~ 
+           m{<a [^>]+map\.yahoo\.co\.jp/address\?ac=(\d+)[^>]+>(.+?)</a>}g) {
+        push(@codes, [$1, $2]);
+    }
+    die "can't determine codes: " . join(',', @codes) if(@codes != 1);
 
-	my ($code, $corrected_shiku) = @{ $codes[0] };
+    my ($code, $corrected_shiku) = @{ $codes[0] };
 
-	# 都道府県の部分を除く
-	my $regexp = quotemeta($tdfk);
-	$corrected_shiku =~ s/^$regexp//;
+    # chop TO-DO-HU-KEN part
+    my $regexp = quotemeta($tdfk);
+    $corrected_shiku =~ s/^$regexp//;
 
-	return ($code, $corrected_shiku);
+    return ($code, $corrected_shiku);
 }
 
 
-# 概要: 市区に対応するフリガナの一覧を得る
-# 引数: 市区ID
-# 戻値: ハッシュリファレンス {漢字1 => かな1, 漢字2 => かな2, ...}
+# get address(kanji) and kana mapping in given SHI-KU (or TO-DO-HU-KEN)
+# params: SHI-KU ID (or TO-DO-HU-KEN ID)
+# return: kanji-kana mapping hash reference: {kanji1 => kana1, kanji2 => kana2}
 sub _get_kana_dict{
-	my $self = shift;
-	my $shiku_code = shift;
-	my $url = sprintf($kana_url_tpl, $shiku_code);
+    my $self = shift;
+    my $shiku_code = shift;
+    my $url = sprintf($kana_url_tpl, $shiku_code);
 
-	my $ua  = $self->{ua};
-	my $res = $ua->get($url);
-	my $c = $res->content();
-	my %ret = ();
-	while ($c =~ m{<dd><div class="ruby">(.+?)</div>(.+?)</dd>}g) {
-		my ($kana, $kanji) = $self->_strip_tag($1, $2);
-		$ret{$kanji} = $kana;
-	}
-	return \%ret;
+    my $ua  = $self->{ua};
+    my $res = $ua->get($url);
+    my $c = $res->content();
+    my %ret = ();
+    while ($c =~ m{<dd><div class="ruby">(.+?)</div>(.+?)</dd>}g) {
+        my ($kana, $kanji) = $self->_strip_tag($1, $2);
+        $ret{$kanji} = $kana;
+    }
+    return \%ret;
 }
 
 
-# 概要: 県名、市区、町字に対するかなを返す
-# 引数: 県名、市区、町字
-# 戻値: けんめい、しく、ちょうあざ
+# get kana of the address
+# params: TO-DO-HU-KEN, SHI-KU, CHO-AZA
+# return: TO-DO-HU-KEN(kana), SHI-KU(kana), CHO-AZA(kana)
 sub search{
-	my $self = shift;
-	my ($tdfk, $shiku, $choaza) = @_;
+    my $self = shift;
+    my ($tdfk, $shiku, $choaza) = @_;
 
-	# 仮名を得るために市区IDが必要。副作用でYahoo表現の市区も手に入る。
-	my ($shiku_code, $corrected_shiku) = $self->_correct_shiku($tdfk, $shiku);
-	my $tdfk_code = substr($shiku_code, 0, 2);
+    # We need ID of SHIKU and TODOHUKEN to get kana.
+    # We can also get SHIKU(Yahoo Exp.) incidentally.
+    my ($shiku_code, $corrected_shiku) = $self->_correct_shiku($tdfk, $shiku);
+    my $tdfk_code = substr($shiku_code, 0, 2);
 
-	my $ref_choaza = $self->_get_kana_dict($shiku_code);
-	my $ref_shiku  = $self->_get_kana_dict($tdfk_code);
+    my $ref_choaza = $self->_get_kana_dict($shiku_code);
+    my $ref_shiku  = $self->_get_kana_dict($tdfk_code);
 
-	# 市区
-	my $shiku_kana = $ref_shiku->{$corrected_shiku};
+    # SHI-KU
+    my $shiku_kana = $ref_shiku->{$corrected_shiku};
 
-	# 町字
-	my $corrected_choaza = '';
-	my $choaza_kana = '';
-	if(exists $ref_choaza->{$choaza}){
-	    # 変換せずに見つかった
-	    $choaza_kana = $ref_choaza->{$choaza};
-	}else{
-	    # Yahoo表現に変換し、再チャレンジ
-	    $corrected_choaza = $self->_correct_choaza(
-	                           $tdfk, $corrected_shiku, $choaza);
-	    $choaza_kana = $ref_choaza->{$corrected_choaza} 
-	                             if exists $ref_choaza->{$corrected_choaza};
-	}
+    # CHO-AZA
+    my $corrected_choaza = '';
+    my $choaza_kana = '';
+    if(exists $ref_choaza->{$choaza}){
+        # the kana was found fortunately.
+        $choaza_kana = $ref_choaza->{$choaza};
+    }else{
+        # retry by using Yahoo Expression
+        $corrected_choaza = $self->_correct_choaza(
+            $tdfk, $corrected_shiku, $choaza);
+        $choaza_kana = $ref_choaza->{$corrected_choaza} 
+        if exists $ref_choaza->{$corrected_choaza};
+    }
 
-	die sprintf("can't read: input(%s, %s), search(%s, %s)", 
-	            $shiku, $choaza, $corrected_shiku, $corrected_choaza)
-	            if ! $shiku_kana or ! $choaza_kana;
+    die sprintf("can't read: input(%s, %s), search(%s, %s)", 
+                $shiku, $choaza, $corrected_shiku, $corrected_choaza)
+        if ! $shiku_kana or ! $choaza_kana;
 
-	return $_tdfk_dict{$tdfk}, $shiku_kana,$choaza_kana;
+    return $_tdfk_dict{$tdfk}, $shiku_kana,$choaza_kana;
 }
 
 
@@ -309,35 +307,3 @@ C<hiratara@cpan.org>, or through the web interface at L<http://rt.cpan.org>.
 =head1 AUTHOR
 
 Masahiro Honma  C<< <hiratara@cpan.org> >>
-
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2008, Masahiro Honma C<< <hiratara@cpan.org> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
-
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
